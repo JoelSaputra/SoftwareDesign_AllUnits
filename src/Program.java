84c93b6aa94c4d888156dd33b36e161fb3e13515
Your full name <joel.clenn03@gmail.com>
2026-04-20 21:25:45 -0400

    Chapter 8 -- Question 3 -- Subject/Observable
    Here we play around with our Subject or Observable or Model which is the Program class as it implements ShowSubject.
    
    This show subject aggregates the Observer by saving a list of Observers as its field.
    
    Now we have to have the method of adding Observers, removing Observers, and notifying observers. Adding observers and Removing observers are trivial as it is just adding and removing an element from an ArrayList.  For notifying, this goes to the heart of Observe design pattern, we use the notifyObservers() method as a callback to the update method we have on the Observer interface. This way we can call update() to all concrete observers (from our list) just by activating the notifyObservers() method in our Subject.
    
    Additionally if there is a change of state from our methods of the model we also have to notify and update the observers thus we call notifyObservers in each of our method calls.

--- src/Program.java
+++ src/Program.java
@@ -3,20 +3,43 @@ import java.util.EnumMap;
 import java.util.List;
 
 class Program implements ShowSubject{
+
+    private final List<ShowObserver> aObservers = new ArrayList<>();
+    private final EnumMap<Day, Show> aShows = new EnumMap<>(Day.class);
+
+
+    @Override
+    public void attach(ShowObserver pObserver) {
+        assert pObserver != null;
+        aObservers.add(pObserver);
+    }
+
+    @Override
+    public void detach(ShowObserver pObserver) {
+        assert pObserver != null;
+        aObservers.remove(pObserver);
+    }
+
+    @Override
+    public void notifyObservers(Day day, String action, Show show) {
+        for(ShowObserver showObserver : aObservers){
+            showObserver.update(day, action, show);
+        }
+    }
+
     public enum Day {
         MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
     }
 
-    // TODO: Add list of observers
-    // TODO: Add observer management methods
+
 
     public void add(Show pShow, Day pDay) {
         System.out.println("Adding show: " + pShow.description() + " on " + pDay);
-        // TODO: Notify observers
+        notifyObservers(pDay, "add", pShow);
     }
 
     public void remove(Day pDay) {
         System.out.println("Removing show on " + pDay);
-        // TODO: Notify observers
+        notifyObservers(pDay, "Removed", aShows.get(pDay));
     }
 }
