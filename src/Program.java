a907a3148f4a9fcb305ba76ea3f4d399aa87ecd7
Your full name <joel.clenn03@gmail.com>
2026-04-20 21:51:29 -0400

    Chapter 8 -- Question 9 -- NULL_SHOW
    Made a singleton null object to represent a null object.
    
    What I did was create an inner class that is private and static that implements Show interface. Then I used the INSTANCE keyword in a way to make a new object of that inner class. The INSTANCE object has to be private static and final. The constructor of this Singelton object has to be private and I created a public static NullShow getInstance() that returns this INSTANCE. Outside of the inner class (in the original program class), we create a referenc to this INSTANCE by using private static final Show NULL_SHOW = NullShow.getInstance();

--- src/Program.java
+++ src/Program.java
@@ -8,6 +8,40 @@ class Program implements ShowSubject{
     private final EnumMap<Day, Show> aShows = new EnumMap<>(Day.class);
 
 
+    private static class NullShow implements Show{
+
+        private static final NullShow INSTANCE = new NullShow();
+
+        private NullShow(){}
+
+        public static NullShow getInstance(){
+            return INSTANCE;
+        }
+
+        @Override
+        public String title() {
+            return "";
+        }
+
+        @Override
+        public String description() {
+            return "No Show";
+        }
+
+        @Override
+        public int time() {
+            return 0;
+        }
+
+        @Override
+        public Show copy() {
+            return this;
+        }
+    }
+
+    private static final Show NULL_SHOW = NullShow.getInstance();
+
+
     @Override
     public void attach(ShowObserver pObserver) {
         assert pObserver != null;
@@ -40,6 +74,12 @@ class Program implements ShowSubject{
 
     public void remove(Day pDay) {
         System.out.println("Removing show on " + pDay);
+        aShows.put(pDay, NULL_SHOW);
         notifyObservers(pDay, "removed", aShows.get(pDay));
     }
+
+
+
+
+
 }
